92c6d4c9b6e2b2e5549d3337dd903153ddf00f3d
Daniel Debian <edramirezd@alumno.uspg.edu.gt>
2025-04-13 00:01:27 -0600

    ejercicios repaso beta

---
+++ src/ejercicio49.js
@@ -0,0 +1,12 @@
+/*
+49. Combinar listas de invitados
+
+Tema: concat()
+
+const invitadosA = ['Carlos', 'Lucía']
+const invitadosB = ['Pedro', 'Sofía']
+Combina ambos arreglos en uno nuevo y muéstralo.
+ */
+
+const listaCombinada = invitadosA.concat(invitadosB);
+console.log('Lista combinada de invitados:', listaCombinada);
---
+++ src/ejercicio50.js
@@ -0,0 +1,12 @@
+/*
+50. Buscar posición de un color
+
+Tema: indexOf()
+
+const colores = ['Rojo', 'Verde', 'Azul', 'Amarillo']
+Encuentra y muestra la posición de "Azul" dentro del arreglo.
+ */
+const colores = ['Rojo', 'Verde', 'Azul', 'Amarillo']
+
+const posicionAzul = colores.indexOf('Azul');
+console.log(`La posición de "Azul" en el arreglo es: ${posicionAzul}`);
